69d69d4f166534a6f1d099c2923d4b02c171a20e
arickert <andysrickert@gmail.com>
2024-04-12 16:56:19 -0700

    styling'

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Baargeld",
+	url: "https://baargeld.de/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Baargeld",
 	author: {
 		name: "Your Name Here",
 		email: "youremailaddress@example.com",
---
+++ content/contributors/index.md
@@ -0,0 +1,7 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: Contributors
+  order: 3
+---
+Jean Arp, Max Ernst, Johannes Theodor Baargeld
